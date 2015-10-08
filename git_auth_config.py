7128b3dfe345d5241b79107100fef60cd2af7263
Niklas Rosenstein <rosensteinniklas@gmail.com>
2015-10-09 01:11:35 +0200

    default root_user should of course be 'root'

--- git_auth_config.py
+++ git_auth_config.py
@@ -6,6 +6,6 @@ import git_auth
 
 git_auth_bin = os.path.join(os.path.dirname(__file__), 'bin', 'git-auth.sh')
 repository_root = os.path.expanduser('~/repos')
-access_controller = git_auth.SimpleAccessController(root_user='niklas')
+access_controller = git_auth.SimpleAccessController(root_user='root')
 ssh_key_manager = git_auth.OpenSSHKeyManager()
 host_name = 'my_git_server'
