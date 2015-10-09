5b884fbde5f45c3a068ecb1bad35eb07fcb88396
Niklas Rosenstein <rosensteinniklas@gmail.com>
2015-10-09 13:52:23 +0200

    git-auth.sh now checks if /etc/profile and /etc/bashrc exist before sourcing them

--- bin/git-auth.sh
+++ bin/git-auth.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-source /etc/profile
-source /etc/bashrc
-if [ -e "~/.bashrc" ]; then
-  source "~/.bashrc"
-fi
-if [ -e "~/.bash_profile" ]; then
-  source "~/.bash_profile"
-fi
+_source_if_exists() {
+  if [ -e "$1" ]; then
+    . "$1"
+  fi
+}
+_source_if_exists /etc/profile
+_source_if_exists /etc/bashrc
+_source_if_exists "~/.bashrc"
+_source_if_exists "~/.bash_profile"
 DIRNAME=$(dirname ${BASH_SOURCE})
 export PYTHONPATH="$DIRNAME/..":$PYTHONPATH
 python3 "$DIRNAME/git-auth.py" $*
