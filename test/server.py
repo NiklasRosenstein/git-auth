dec5468b7b26ae89042cabfefbe5b0a29f13e17b
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-01-26 18:44:08 +0100

    update test server and testcases

--- test/server.py
+++ test/server.py
@@ -1,8 +1,20 @@
 
+import shlex
 import {AuthHandler, GitAuth} from '@NiklasRosenstein/git-auth'
 
+class MyAuthHandler(AuthHandler):
+
+  def authorize_ssh_command(self, request):
+    command = shlex.split(request.command)
+    if len(command) >= 2 and command[0] in ('git-receive-pack', 'git-upload-pack'):
+      repository = command[1]
+      if repository.startswith(request.username + '/'):
+        return request.allow(command='bash -c :')  # noop
+    request.print("You don't have access to this repository.")
+    return request.deny()
+
 server = GitAuth(
-  handler_class = AuthHandler,
+  handler_class = MyAuthHandler,
   authorized_keys_file = 'authorized_keys',
   auth_socket = 'auth.sock',
   prefix = 'repos'
--- test/test.sh
+++ test/test.sh
@@ -5,6 +5,12 @@ SERVER_ID=$!
 sleep 1
 
 SSH_ORIGINAL_COMMAND="git-receive-pack JohnSmith/somerepo.git" \
-  git-auth-client auth.sock --type ssh --username JohnSmith --key-id foo
+  git-auth-client auth.sock --type ssh --username JohnSmith --key-id foo >> /dev/null
+if [ $? != 0 ]; then exit "JohnSmith/somerepo.git should be accessible."; fi
+
+SSH_ORIGINAL_COMMAND="git-receive-pack EpicStuff/epic.git" \
+  git-auth-client auth.sock --type ssh --username JohnSmith --key-id foo >> /dev/null
+if [ $? == 0 ]; then exit "EpicStuff/epic.git should not be accessible."; fi
 
 kill $SERVER_ID
+echo "Successfull in all testcases."
